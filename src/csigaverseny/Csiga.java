becc89fc38d2170cec0539c958f042e16abde4b2
Simon Kende Simon (SZF_2023_2025) <simon.kende.simon@diak.szamalk-szalezi.hu>
2024-04-25 12:04:44 +0200

    Csiga adattagok hozzadasa

--- src/csigaverseny/Csiga.java
+++ src/csigaverseny/Csiga.java
@@ -4,12 +4,19 @@ package csigaverseny;
 
 class Csiga {
     
+    private String csigaVersenyzo;
+    private String csigaSzin;
     private int lepes;
     private int osszlepes;
     
+    
+    
     public Csiga() {
+        this.csigaVersenyzo = csigaVersenyzo;
+        this.csigaSzin = csigaSzin;
         this.osszlepes = osszlepes;
         this.lepes = lepes;
     }
-    JatekTer j = new JatekTer();
+    
+    
 }
