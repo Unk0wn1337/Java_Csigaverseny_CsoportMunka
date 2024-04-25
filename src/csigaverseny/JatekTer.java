7678d9042be16f2b1ef5d7c39ef560c94c252313
Simon Kende Simon (SZF_2023_2025) <simon.kende.simon@diak.szamalk-szalezi.hu>
2024-04-25 12:11:29 +0200

    JatekTer adattagok megadasa

--- src/csigaverseny/JatekTer.java
+++ src/csigaverseny/JatekTer.java
@@ -1,7 +1,19 @@
 
 package csigaverseny;
 
+import java.util.Scanner;
+
 
 class JatekTer {
-    
+    static Scanner sc = new Scanner(System.in);
+    int tipp;
+    int korSzamolo;
+
+    public JatekTer(int tipp, int korSzamolo) {
+        this.tipp = sc.nextInt();
+        this.korSzamolo = korSzamolo;
+    }
+    void start(){
+        
+    }
 }
