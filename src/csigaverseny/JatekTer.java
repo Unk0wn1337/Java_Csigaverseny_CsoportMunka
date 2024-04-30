4ece22f8125f9164c0f97aaec61f30dc0a7c2a87
Unk0wn1337 <116260492+Unk0wn1337@users.noreply.github.com>
2024-04-30 21:17:56 +0200

    mozgas meghatarozva, ki kell meg az eredmeny hirdetest szervezni uj metodusba

--- src/csigaverseny/JatekTer.java
+++ src/csigaverseny/JatekTer.java
@@ -1,7 +1,6 @@
 
 package csigaverseny;
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,9 +14,7 @@ class JatekTer {
   
     
     public void start(){
-        
-        
-        
+ 
         System.out.println("Hany koros legyen?");
         int korMegadas = sc.nextInt();
         setKorSzamolo(korMegadas);
@@ -50,13 +47,53 @@ class JatekTer {
         cs3.setCsigaVersenyzo(3);
         cs3.setOsszlepes(3);
         
+        int csigaLepesSzamlalo1 = 0;
+        int csigaLepesSzamlalo2 = 0;
+        int csigaLepesSzamlalo3 = 0;
         
         Random rnd = new Random();
-        for (int i = 0; i < getKorSzamolo(); i++) {
-             
-            cs1.setLepes(tipp);
+        for (int i = 1; i < getKorSzamolo()+1; i++) {
+            cs1.setLepes(rnd.nextInt(3));
+            cs2.setLepes(rnd.nextInt(3));
+            cs3.setLepes(rnd.nextInt(3));
+            
+            csigaLepesSzamlalo1 += cs1.getLepes();
+            csigaLepesSzamlalo2 += cs2.getLepes();
+            csigaLepesSzamlalo3 += cs3.getLepes();
+        
+            System.out.println(cs1.getCsigaSzin()+" ("+cs1.getCsigaVersenyzo()+"-es versenyo) "+cs1.getLepes());
+            System.out.println(cs2.getCsigaSzin()+" ("+cs2.getCsigaVersenyzo()+"-es versenyo) "+cs2.getLepes());
+            System.out.println(cs3.getCsigaSzin()+" ("+cs3.getCsigaVersenyzo()+"-asversenyzo) "+cs3.getLepes());
+            System.out.println("------------ Kor: "+i+" ------------");
         }
+        
+        if(csigaLepesSzamlalo1 > csigaLepesSzamlalo2 && csigaLepesSzamlalo1 > csigaLepesSzamlalo3){
+            System.out.println("Piros csiga nyert, lepes szama:"+csigaLepesSzamlalo1);
+            if(getTipp() == cs1.getCsigaVersenyzo()){
+                System.out.println("Nyertel 🍺");
+            } else {
+                System.out.println("Te semmit nem nyertel");
+            }
+        } else if(csigaLepesSzamlalo2 > csigaLepesSzamlalo1 && csigaLepesSzamlalo2 > csigaLepesSzamlalo3){
+            System.out.println("Kek csiga nyert, lepes szama:"+csigaLepesSzamlalo1);
+            if(getTipp() == cs2.getCsigaVersenyzo()){
+                System.out.println("Nyertel");
+            } else {
+                System.out.println("Te semmit nem nyertel ");
+            }
+        } else if(csigaLepesSzamlalo3 > csigaLepesSzamlalo1 && csigaLepesSzamlalo3 > csigaLepesSzamlalo2){
+            System.out.println("Kek csiga nyert, lepes szama:"+csigaLepesSzamlalo1);
+            if(getTipp() == cs3.getCsigaVersenyzo()){
+                System.out.println("Nyertel");
+            } else {
+                System.out.println("Te semmit nem nyertel");
+            }
+        } else {
+            System.out.println("Dontetlen");
+        }
+
     }
+    
     
     
 
